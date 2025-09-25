427fcf2360cb822a04febd22965b760a837d758a
Jaseel <jaseel@Jaseels-iMac.local>
2025-09-25 17:28:49 +0400

    Second

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+require('dotenv').config();
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.DB ?? '')],
   controllers: [AppController],
   providers: [AppService],
 })
